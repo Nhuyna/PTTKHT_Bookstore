5e343d42746ae275caa9e49b0689ab04a1a7e842
ngan23github <ledoankimngan2005@gmail.com>
2025-05-06 14:15:40 +0700

    update

--- src/routes/user/cartRouter.js
+++ src/routes/user/cartRouter.js
@@ -3,11 +3,11 @@ import cartController from "../../app/controllers/user/cartController.js";
 
 const router = express.Router();
 
-router.get("/", cartController.renderCartPage);
 router.post("/thanhtoan", cartController.thanhtoan);
 router.post("/confirm", cartController.afterpayment);
 router.get("/confirm", cartController.renderThankYouPage);
 router.post("/addToCart", cartController.addToCart);
 router.post("/cartCount", cartController.getcartCount);
+router.get("/", cartController.renderCartPage);
 
 export default router;
